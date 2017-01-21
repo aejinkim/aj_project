df3095c58a0f4d272593e6e807d0bd1c1c6232f2
aejinkim <arias0410@aejinkims-MacBook-Pro.local>
2017-01-21 22:42:15 +0900

    Add aj_work

--- src/aj_project/admin.py
+++ src/aj_project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Comment
 
 # Register your models here.
+@admin.register(Article, Comment)
+class aj_projectAdmin(admin.ModelAdmin):
+    pass
--- src/aj_project/views.py
+++ src/aj_project/views.py
@@ -1,7 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 # 장고 안에 데이터 뭉치안에 http안에 httpRespose 도구를 쓴다는 의미
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the aj_project index.")
+    # name = "ajkim"
+    # return render(request, "index.html", {"name": name})
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count=0
+    # )
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
